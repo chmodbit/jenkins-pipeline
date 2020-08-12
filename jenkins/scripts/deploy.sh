39a71bc0a0c1583d3d8cdb7bbff9d0d85a5973a9
Ardityo Mulawarman <ardityo.mulawarman@tokopedia.com>
2020-08-12 09:07:51 +0700

    fix stage

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -15,18 +15,20 @@ docker container run -d \
 
 # Logic to wait for the api component to be ready on port 3000
 
-read -d '' wait_for << EOF
-echo "Waiting for API to listen on port 3000..."
-while ! nc -z api 3000; do 
-  sleep 0.1 # wait for 1/10 of the second before check again
-  printf "."
-done
-echo "API ready on port 3000!"
-EOF
+echo "wait 5s"
+sleep 5
+#read -d '' wait_for << EOF
+#echo "Waiting for API to listen on port 3000..."
+#while ! nc -z api 3000; do 
+#  sleep 0.1 # wait for 1/10 of the second before check again
+#  printf "."
+#done
+#echo "API ready on port 3000!"
+#EOF
 
-docker container run --rm \
-    --net test-net \
-    node:12.10-alpine sh -c "$wait_for"
+#docker container run --rm \
+#    --net test-net \
+#    node:12.10-alpine sh -c "$wait_for"
 
 echo "Smoke tests..."
 docker container run --name tester \
